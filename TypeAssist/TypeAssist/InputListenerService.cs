88d7eae187e544822d6da73dc9e6558251ee4f49
thzeMedic <amer.medic09@gmail.com>
2025-12-14 15:48:46 +0100

    Ghost Overlay works now, global Keybinds for A-Z works

---
+++ TypeAssist/TypeAssist/InputListenerService.cs
@@ -0,0 +1,36 @@
+﻿using DeftSharp.Windows.Input.Keyboard;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace TypeAssist
+{
+    static class InputListenerService
+    {
+        private static KeyboardListener _keyboardListener = new KeyboardListener();
+        private static HashSet<Key> _subscribedKeys = new HashSet<Key>()
+        {
+            Key.A, Key.B, Key.C, Key.D, Key.E, Key.F, Key.G,
+            Key.H, Key.I, Key.J, Key.K, Key.L, Key.M, Key.N,
+            Key.O, Key.P, Key.Q, Key.R, Key.S, Key.T, Key.U,
+            Key.V, Key.W, Key.X, Key.Y, Key.Z,
+            Key.D0, Key.D1, Key.D2, Key.D3, Key.D4,
+            Key.D5, Key.D6, Key.D7, Key.D8, Key.D9
+        };
+
+        public static void Subscribe(TextBlock block)
+        {
+            foreach (var key in _subscribedKeys)
+            {
+                _keyboardListener.Subscribe(key, key => block.Text = $"Key pressed: {key}");
+            }
+        }
+
+        public static void Unsubscribe()
+        {
+            _keyboardListener.Unsubscribe(System.Windows.Input.Key.A);
+        }
+    }
+}
--- TypeAssist/TypeAssist/MainWindow.xaml.cs
+++ TypeAssist/TypeAssist/MainWindow.xaml.cs
@@ -1,24 +1,28 @@
-﻿using System.Text;
+﻿using DeftSharp.Windows.Input.Keyboard;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace TypeAssist
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
+
+        private static KeyboardListener _keyboardListener = new KeyboardListener();
+
         public MainWindow()
         {
             InitializeComponent();
+            InputListenerService.Subscribe(testblock);
+
+        }
+
+        private void Window_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine($"Key pressed: {e.Key}");
+            testblock.Text = $"Key pressed: {e.Key}";
         }
     }
 }
